9713a053d4b8ce54bcb172aa3884b46ff06a8892
Idris8303 <bctxtoast@gmail.com>
2017-07-26 17:01:50 -0500

    first push

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mustache = require('mustache-express');
-
+const data = require('./data');
 const app = express();
 app.engine('mustache', mustache());
 
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  res.render('index')
+  res.render('index', data)
+});
+
+app.get('/details/:id',function(req, res){
+
+
+
+  let Users = data.users.find(function(item){
+    return item.id == req.params.id;
+  });
+  res.render('details', person);
+
 });
 
 
